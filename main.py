e02df402deeb384764e87a974266fd380565e11d
Kapoc <cristiancamilo1033@gmail.com>
2021-04-26 19:35:17 -0500

    pycharm

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 from app.logic.SpeedTest import SpeedTestLogic
 from app.utils.Converter import Converter
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     size, label = Converter.format_bytes(SpeedTestLogic.get_upload_speed())
     print("size {0}, label {1}", size, label)
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
